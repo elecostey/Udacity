0e0aa89c7be025e2ec46510d8ec5473f4ddd2be3
Elodie Lecostey <lecostey.e@gmail.com>
2017-01-28 16:04:04 +0100

    - added new project

---
+++ ud989-cat-clicker-ko-starter-master/js/app.js
@@ -0,0 +1,15 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.mgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+
+
+}
+
+
+ko.applyBindings(new ViewModel());
