0e5caf3bdc42763bb09dc562a9fd286882111ea9
Elodie Lecostey <lecostey.e@gmail.com>
2017-01-29 18:09:29 +0100

    - cat clicker with knockout.js

--- ud989-cat-clicker-ko-starter-master/js/app.js
+++ ud989-cat-clicker-ko-starter-master/js/app.js
@@ -1,13 +1,81 @@
-var ViewModel = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.mgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+var initialCats = [
+    {
+        clickCount: 0,
+        name: "Rengo",
+        imgSrc: "img/rengo.jpg",
+        imgAttribution: "Somebody",
+        nickNames: ['Rengar', 'Rengui', 'Rengach']
+    },
+    {
+        clickCount: 0,
+        name: "Maru",
+        imgSrc: "img/maru.jpg",
+        imgAttribution: "Somebody",
+        nickNames: ['Tiger', 'Hero', 'Cat']
+    },
+    {
+        clickCount: 0,
+        name: "Romeo",
+        imgSrc: "img/romeo.jpg",
+        imgAttribution: "Somebody",
+        nickNames: ['Tiger', 'Hero', 'Cat']
+    },
+    {
+        clickCount: 0,
+        name: "Ulfie",
+        imgSrc: "img/ulfie.jpg",
+        imgAttribution: "Somebody",
+        nickNames: ['Tiger', 'Hero', 'Cat']
+    },
+    {
+        clickCount: 0,
+        name: "Bobi",
+        imgSrc: "img/bobi.jpg",
+        imgAttribution: "Somebody",
+        nickNames: ['Tiger', 'Hero', 'Cat']
+    }
+];
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
 
+var ViewModel = function(){
+	var self = this;//self always maps to ViewModel
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem){
+		self.catList.push(new Cat(catItem));
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
+
+	this.incrementCounter = function(){
+		this.clickCount(this.clickCount() + 1);
+	};
+
+	this.changeCurrentCat = function(clickedCat){
+		self.currentCat(clickedCat)
+	};
+};
+
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.nickNames = ko.observableArray(data.nickNames);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.level = ko.computed(function(){
+        if (this.clickCount() < 5){
+            level = "Newborn";
+            return level;
+        } else if (this.clickCount() < 10){
+            level = "Infant";
+            return level;
+        } else {
+            level = "Teen";
+            return level;
+        }
+    },this);
 
 }
 
