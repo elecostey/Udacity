aa33d542ce3ac54d62b9ef84a5e4f140eb8b7c30
Elodie Lecostey <lecostey.e@gmail.com>
2017-01-30 15:58:43 +0100

    - first commit neighbourhood map project

---
+++ neighorhood-map/js/app.js
@@ -0,0 +1,35 @@
+var map;
+      function initMap() {
+        var trnsko = {lat: 45.773113, lng: 15.964594};
+        var caffeBarPepper = {lat: 45.772963, lng: 15.964991};
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: trnsko,
+          zoom: 15
+        });
+
+        var marker = new google.maps.Marker({
+          position: caffeBarPepper,
+          map: map,
+          title: 'Caffe Bar Pepper'
+        });
+
+        var contentString = '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h2 id="firstHeading" class="firstHeading">Caffe Bar Pepper</h2>'+
+          '<div id="bodyContent">'+
+          '<h4>Pizzeria</h4>'+
+          '<p>Trnsko ul. 29C</p>'+
+          '<p>10000, Zagreb</p>'+
+          '<p>Croatia</p>'+
+          '</div>'+
+          '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
+
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+          });
+      }
